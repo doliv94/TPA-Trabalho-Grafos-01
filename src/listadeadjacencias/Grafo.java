7046ba6eca67ab74dcf180330a08b6e43287a446
doliv94 <bdalzini@gmail.com>
2022-12-10 09:33:22 -0300

    .

--- src/listadeadjacencias/Grafo.java
+++ src/listadeadjacencias/Grafo.java
@@ -427,57 +427,10 @@ public class Grafo<T> {
         return retorno;
     }
 
-    private boolean montaCaminho(ArrayList<Vertice<T>> fila, Vertice<T> proxVertice, Vertice<T> origem, Vertice<T> destino) {
-        ArrayList<Vertice<T>> caminho = new ArrayList<>();
-        float fluxoMax = 0.0f;
-        fila.add(proxVertice);
-
-        for (int j = 0; j < proxVertice.getDestinos().size(); j++) {
-
-            if (proxVertice.getDestinos().get(j).getPeso() > 0 && proxVertice.getDestinos().get(j).getDestino() != origem) {
-                if (proxVertice.getDestinos().get(j).getDestino() == destino) {
-
-
-                    return true;
-                }
-                else {
-                    if (proxVertice.getDestinos().get(j).getDestino() != origem && !fila.contains(proxVertice.getDestinos().get(j).getDestino())) {
-                        if (montaCaminho(fila, proxVertice.getDestinos().get(j).getDestino(), origem, destino)) {
-
-                            if (!caminho.contains(origem)) {
-                                caminho.add(0, origem);
-                                caminho.add(caminho.size() - 1, proxVertice.getDestinos().get(j).getDestino());
-                                caminho.add(caminho.size(), destino);
-                            }
-                            caminho.add(1, proxVertice);
-
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-
-
-
-        return false;
-    }
-
     // Metodo para determinar o fluxo maximo entre dois vertices
     public void calculaFluxoMaximo (Vertice<T> origem, Vertice<T> destino) {
-        boolean caminho;
-        Vertice<T> proxVertice = new Vertice<>();
-        ArrayList<Vertice<T>> fila = new ArrayList<>();
 
-        for (int i = 0; i < origem.getDestinos().size(); i++) {
 
-            if (origem.getDestinos().get(i).getPeso() > 0) {
-                proxVertice = origem.getDestinos().get(i).getDestino();
-                caminho = montaCaminho(fila, proxVertice, origem, destino);
-            }
-        }
-
-        // segue um caminho -> pegar o menor peso das arestas de ida -> faz o balanceamento no sentido inverso
 
     }
 }
